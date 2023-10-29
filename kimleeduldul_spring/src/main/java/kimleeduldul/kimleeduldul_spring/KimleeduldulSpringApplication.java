7dc3cd3bd6ebaab6c71918a42f1a0c6aa51fa3bc
programofktw <thkim000317@naver.com>
2023-10-29 15:15:37 +0900

    2023/10/29 프로젝트 시작

---
+++ kimleeduldul_spring/src/main/java/kimleeduldul/kimleeduldul_spring/KimleeduldulSpringApplication.java
@@ -0,0 +1,13 @@
+package kimleeduldul.kimleeduldul_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KimleeduldulSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KimleeduldulSpringApplication.class, args);
+	}
+
+}
---
+++ kimleeduldul_spring/src/test/java/kimleeduldul/kimleeduldul_spring/KimleeduldulSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package kimleeduldul.kimleeduldul_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KimleeduldulSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
